417fe8ba194954b73c60ef89ed330eb3137d2331
pssgiasmim <iasmimpassing@gmail.com>
2024-10-01 20:53:02 -0300

    Método Start de LightTower

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/LightTower.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/LightTower.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightTower : MonoBehaviour
+public class LightTower : TowerBase
 {
-    // Start is called before the first frame update
-    void Start()
+    //Método responsável por organizar informações de LightTower
+    public void Start()
     {
-        
+        TaxaDeAtaque = 0.5f; //A taxa de ataque é de 0.5f
+        Alcance = 6.0f; //O alcance/distância é de 6f
+        Dano = 15; //O valor do dano é 15
+        TipoDeDano = "light"; //O tipo de dano é luz
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
