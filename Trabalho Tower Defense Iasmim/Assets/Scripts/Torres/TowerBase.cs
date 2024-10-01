6a69da7099fff654f122237945916a493a901d32
pssgiasmim <iasmimpassing@gmail.com>
2024-10-01 19:43:44 -0300

    Criação de variáveis da TowerBase

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/TowerBase.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/TowerBase.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerBase : MonoBehaviour
+public class TowerBase : MonoBehaviour, IAtacavel
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Esta classe serve de base para todas as outras torres.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float TaxaDeAtaque; //Variável da Taxa de ataque da torre.
+    public float Alcance; //Variável de Alcance da torre.
+    public int Dano; //Variável de Dano da torre.
+    public string TipoDeDano; //Variável do Tipo de dano da torre.
+    private float UltimoAtaque; //Variável que registra o último ataque da torre.
+
+
 }
