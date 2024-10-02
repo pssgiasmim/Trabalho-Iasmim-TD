40b64797f2943cd66fff801591f5ffb29a084c1d
pssgiasmim <iasmimpassing@gmail.com>
2024-10-02 18:57:28 -0300

    Método ReceberDano de EnemyBase
    Corrigindo o Commit anterior, a classe não se chama Inimigos, mas sim EnemyBase

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Inimigos/EnemyBase.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Inimigos/EnemyBase.cs
@@ -9,4 +9,15 @@ public class EnemyBase : MonoBehaviour, IAtacavel
     public int saude; //Variável que "conta" a vida do inimigo.
     public string tipoDeInimigo; //Variável que vê qual é o tipo de inimigo.
 
+    //Método chamado quando o inimigo receber dano.
+    public void ReceberDano(int quantidade)
+    {
+        saude -= quantidade; //saude inicia sendo menor ou igual a quantidade.
+
+        //Se saude for menor ou igual a zero, então
+        if (saude <= 0)
+        {
+            Morrer(); //chamando o método morrer.
+        }
+    }
 }
