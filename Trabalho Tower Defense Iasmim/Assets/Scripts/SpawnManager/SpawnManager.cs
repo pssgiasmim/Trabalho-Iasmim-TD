c5a54858ca5cc2187724c57a8ce6f0cc15026035
pssgiasmim <iasmimpassing@gmail.com>
2024-10-07 14:30:39 -0300

    Criação das Variáveis

--- Trabalho Tower Defense Iasmim/Assets/Scripts/SpawnManager/SpawnManager.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/SpawnManager/SpawnManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    
-    
-    
+    [SerializeField] GameObject EnemyFire; //Variável que armazena no inspector o EnemyFire.
+    [SerializeField] GameObject EnemyIce; //Variável que armazena no inspector o EnemyIce.
+    [SerializeField] GameObject EnemyDark; //Variável que armazena no inspector o EnemyDark.
+    [SerializeField] GameObject EnemyLight; //Variável que armazena no inspector o EnemyLight.
+    [SerializeField] GameObject EnemyRock; //Variável que armazena no inspector o EnemyRock                                        .
+    float timer = 1.2f; //Variável que é um timer para saber o momento certo de começar a instanciar.
+    int TipoInimigo; //Variável que tem um inimigo aleatório que servirá como condição para instanciar.
+
+
     //Método que vai Spawnar os inimigos na cena, em direção das torres.
     public void Spawn()
     {
