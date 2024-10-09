d3b44b0bf629a8906ffa17e88d978b82290afce4
pssgiasmim <iasmimpassing@gmail.com>
2024-10-09 14:26:50 -0300

    Tentativa de fazer as torres serem selecionadas

--- Trabalho Tower Defense Iasmim/Assets/Scripts/PosiçãoTorres/TowerButton.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/PosiçãoTorres/TowerButton.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BotaoTorres : MonoBehaviour
+//Classe que gerencia os botões das torres
+public class TowerButton : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
