8e7c2de505b829be1cf3b710af79470f1c135c65
pssgiasmim <iasmimpassing@gmail.com>
2024-10-09 13:57:54 -0300

    Tentativa 1

--- Trabalho Tower Defense Iasmim/Assets/Scripts/PosiçãoTorres/TowerPlace.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/PosiçãoTorres/TowerPlace.cs
@@ -5,8 +5,29 @@ using UnityEngine;
 //Classe que faz com que o jogador escolha onde posicionar as torres.
 public class TowerPlace : MonoBehaviour
 {
-    public GameObject towerPrefab; //Variável do prefab da torre.
+    public GameObject fireTowerPrefab; //Variável do prefab da torre de fogo.
+    public GameObject iceTowerPrefab; //Variável do prefab da torre de gelo.
+    public GameObject lightTowerPrefab; //Variável do prefab da torre de luz.
     private Camera mainCamera; //Variável que faz com que a camerâ "acompanhe" do jogador.
 
+    //Método que pega a camera principal.
+    public void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
+    //Método que detecta o clique do mouse.
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition; //Variável que recebe a posição do mouse.
+            mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
+            mousePosition.z = 0; // Define a posição z para 0 em 2D.
+
+            Instantiate(fireTowerPrefab, mousePosition, Quaternion.identity);
+            Instantiate(iceTowerPrefab, mousePosition, Quaternion.identity);
+            Instantiate(lightTowerPrefab, mousePosition, Quaternion.identity);
+        }
+    }
 }
