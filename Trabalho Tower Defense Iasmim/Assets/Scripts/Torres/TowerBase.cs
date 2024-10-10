f26438c250ecd4ab7a2f710b76d34e715e868f36
pssgiasmim <iasmimpassing@gmail.com>
2024-10-10 13:50:46 -0300

    Método Atacar do Ataque das Torres

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/TowerBase.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/TowerBase.cs
@@ -2,19 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Esta classe serve de base para todas as outras torres.
+
 public class TowerBase : MonoBehaviour, IAtacavel
 {
-    //Esta classe serve de base para todas as outras torres.
-
     public float TaxaDeAtaque; //Variável da Taxa de ataque da torre.
     public float Alcance; //Variável de Alcance da torre.
     public int Dano; //Variável de Dano da torre.
     public string TipoDeDano; //Variável do Tipo de dano da torre.
-    private float UltimoAtaque; //Variável que registra o último ataque da torre.
+    public float UltimoAtaque; //Variável que registra o último ataque da torre.
+    public GameObject bombaPrefab; //Variável do prefab da bomba.
+    public Transform pontoDeTiro; //Variável do ponto de onde a bomba vai ser disparada.
 
+    
     //Método que tem como função atacar os inimigos.
     public virtual void Atacar(List<EnemyBase> enemiesInRange)
     {
-        //Dentro dos scripts das torres especificas, estará o detalhamento.
+        if (Time.time >= UltimoAtaque + TaxaDeAtaque)
+        {
+            foreach (EnemyBase enemy in enemiesInRange)
+            {
+                int dano = Dano; //Variável dano que recebe o valor de Dano.
+
+                if (enemy.tipoDeInimigo == "Dark" && this is FireTower)
+                {
+                    dano *= 2;
+                }
+
+                else if (enemy.tipoDeInimigo == "Fire" && this is IceTower)
+                {
+                    dano *= 2;
+                }
+
+                else
+                {
+                    dano = 1;
+                }
+
+                DispararBola(dano, enemy);
+
+                break;
+            }
+
+            UltimoAtaque = Time.time;
+        }
     }
 }
