0370c0589a674815baf6602217c2c309e8dc7c8f
pssgiasmim <iasmimpassing@gmail.com>
2024-10-15 15:25:12 -0300

    Comentários

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Interfaces/IAtacavel.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Interfaces/IAtacavel.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Define o comportamento de atacar, que pode ser usado pela torre ou inimigos
 public interface IAtacavel 
 {
-    //Define o comportamento de ataque ao inimigo
-
-    //Método que ataca o inimigo
+    //Método atacar que faz com que algum objeto ataque outro, de acordo com o alcance.
     public void Atacar(EnemyBase enemiesInRange);
 
 }
