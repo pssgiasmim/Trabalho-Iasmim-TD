6cedb591ac380a9fcc10d9622d480ffa5d17cc84
pssgiasmim <iasmimpassing@gmail.com>
2024-10-16 10:29:15 -0300

    Comentários de EnemyBase

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Inimigos/EnemyBase.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Inimigos/EnemyBase.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Classe Base dos inimigos. Todos os cinco inimigos herdam desta classe.
+/*Classe Base dos inimigos. Todos os cinco inimigos herdam desta classe.
+ * Aqui nós fazemos os inimigos receberem o dano.
+ * Após receber o dano, eles são destruidos / morrem.
+ * Método que faz os inimigos atacarem , esta inativo.
+ * Método update que faz o inimigo vir numa velocidade e consequentemente ser instanciado na cena.
+ */
 
 public class EnemyBase : MonoBehaviour, IAtacavel
 {
@@ -12,7 +17,10 @@ public class EnemyBase : MonoBehaviour, IAtacavel
     [SerializeField] float speed; //Variável que é a velocidade dos inimigos.
     [SerializeField] Rigidbody2D rigidbody2D; //Variável que cuida da fisica dos inimigos.
 
-    //Método chamado quando o inimigo receber dano.
+    /* Método chamado quando o inimigo receber dano.
+     * Ele subtrai a "saude" do inimigo pela "quantidade" de dano recebido.
+     * verifica se a "saude" é menor do que 0 para destruir o inimigo com o método "Morrer()".
+     */
     public void ReceberDano(int quantidade)
     {
         saude -= quantidade; 
@@ -30,13 +38,15 @@ public class EnemyBase : MonoBehaviour, IAtacavel
         Destroy(gameObject); 
     }
 
-    //Método que faz os inimigos atacarem as torres.
+    //Método que faz os inimigos atacarem as torres, está inativo.
     public virtual void Atacar(EnemyBase enemiesInRange)
     {
         gameObject.SetActive(false);
     }
 
-    //Método que faz os inimigos se moverem no mapa.
+    /* Método que faz os inimigos se moverem no mapa.
+     * Pega a velocidade do rigidbody do inimigo e faz ele se mover para o lado esquerdo (-1) sem mexer na altura (0).
+     */
     public void Update()
     {
         rigidbody2D.velocity = new Vector2(-1, 0);
