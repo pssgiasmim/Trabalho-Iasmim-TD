874b150637c9e3632d1459f56e1537cd1800a20c
pssgiasmim <iasmimpassing@gmail.com>
2024-10-16 14:40:37 -0300

    Organização de cena

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/IceTower.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/IceTower.cs
@@ -10,18 +10,5 @@ public class IceTower : TowerBase
         
     }
 
-    /*/Elaboração do ataque da Torre de Gelo
-    public override void Atacar(List<EnemyBase> enemiesInRange)
-    {
-        //Para cada Inimigo em inimigoEmPerigo
-        foreach (EnemyBase enemy in enemiesInRange)
-        {
-            //Se o tipo do inimigo for igual a fogo
-            if (enemy.tipoDeInimigo == "fogo") //Esta torre só ataca inimigos do tipo fogo.
-            {
-                //O inimigo "pega" o dano.
-                enemy.ReceberDano(Dano);
-            }
-        }
-    }*/
+   
 }   
--- Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/LightTower.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/LightTower.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 
 public class LightTower : TowerBase
 {
-    //Método responsável por organizar informações de LightTower
+    //Método que faz
     public void Start()
     {
         
     }
 
-    /*/Elaboração do ataque da Torre de Luz
-    public override void Atacar(List<EnemyBase>enemiesInRange)
-    {
-        //Para cada Inimigo em inimigoEmPerigo
-        foreach(EnemyBase enemy in enemiesInRange)
-        {
-            //Aqui ataca todos os inimigos.
-            enemy.ReceberDano(Dano);
-        }
-    }*/
 
     //modificar o método atacar para chamar disparar bomba duas vezes
 }
