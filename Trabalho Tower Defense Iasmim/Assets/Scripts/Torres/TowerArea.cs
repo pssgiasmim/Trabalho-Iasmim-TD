cb9b245a606e14547846dd70bfdd2cc4e16c8d23
pssgiasmim <iasmimpassing@gmail.com>
2024-10-21 13:27:17 -0300

    Itens do script de TowerArea

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/TowerArea.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Torres/TowerArea.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Classe responsável por verificar por meiop da tag Torre, se alguma torre está dentro da área ou não.
 public class TowerArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool estaOcupado = false; //Variável que armazena se o local está ocupado ou não.
+
+    //Método que verifica de a torre está dentro da área, utilizando uma tag Torre, que serve como "verificação".
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Torre"))
+        {
+            estaOcupado=true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //Método que verifica se a torre ainda está na área ou não.
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Torre"))
+        {
+            estaOcupado = false;
+        }
     }
+
 }
