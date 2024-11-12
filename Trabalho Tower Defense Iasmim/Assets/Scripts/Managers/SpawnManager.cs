59304339081a39debdd7ff3dba3db367666a6d1f
pssgiasmim <iasmimpassing@gmail.com>
2024-11-12 14:56:35 -0300

    Fazer inimigos avançarem em grupos na cena

--- Trabalho Tower Defense Iasmim/Assets/Scripts/Managers/SpawnManager.cs
+++ Trabalho Tower Defense Iasmim/Assets/Scripts/Managers/SpawnManager.cs
@@ -11,8 +11,12 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] GameObject EnemyIce; //Variável que armazena no inspector o EnemyIce.
     [SerializeField] GameObject EnemyDark; //Variável que armazena no inspector o EnemyDark.
     [SerializeField] GameObject EnemyLight; //Variável que armazena no inspector o EnemyLight.
-    [SerializeField] GameObject EnemyRock; //Variável que armazena no inspector o EnemyRock                                        .
-    float timer = 3.0f; //Variável que é um timer para saber o momento certo de começar a instanciar.
+    [SerializeField] GameObject EnemyRock; //Variável que armazena no inspector o EnemyRock
+                                           //.
+    float timer = 10.0f; //Tempo entre os grupos de inimigos;
+    float tempoDeSpawn = 0f; //Temporizador para controlar o spawn dos grupos;
+    int inimigosSpawnados = 0; //Contador de grupos que já foram spawnados;
+    int tamanhoDoGrupo = 10; //Quantidade de inimigos por grupo;
     int tipoInimigo; //Variável que tem um inimigo aleatório que servirá como condição para instanciar.
     int spawnPlace; //Variável que tem um valor aleatório  que verá a posição de onde o inimigo será instanciado.
     float y; //Variáveis que irão armazenar as posições. X é 7.85 e Y é aleatório.
@@ -32,73 +36,73 @@ public class SpawnManager : MonoBehaviour
     //Método que vai Spawnar os inimigos na cena, em direção das torres.
     public void Spawn()
     {
-        if (timer <= 0)
-        {
-            timer = 1.0f;
+       if (tempoDeSpawn <= 0 && inimigosSpawnados < tamanhoDoGrupo) // Se o tempo estiver zerado e ainda houver inimigos para spawnar no grupo
+       {
+            
+            spawnPlace = Random.Range(1, 4);
 
-            if (GameManager.instance.enemies.Count < 25)
+            if (spawnPlace == 1)
             {
-                spawnPlace = Random.Range(1, 4);
-
-                if (spawnPlace == 1)
-                {
                     y = 2.89f;
-                }
-
-                else if (spawnPlace == 2)
-                {
-                    y = -0.23f;
-                }
-
-                else if (spawnPlace == 3)
-                {
-                    y = -3.27f;
-                }
-
-                tipoInimigo = Random.Range(0, 100);
-
-                if (tipoInimigo > 70)
-                {
-                    GameObject obj = Instantiate(EnemyFire, new Vector3(7.85f, y, 0), Quaternion.identity);
-                    GameManager.instance.AdicionarInimigos(obj);
-                }
-
-                else if (tipoInimigo > 50)
-                {
-                    GameObject obj = Instantiate(EnemyDark, new Vector3(7.85f, y, 0), Quaternion.identity);
-                    GameManager.instance.AdicionarInimigos(obj);
-                }
-
-                else if (tipoInimigo > 10)
-                {
-                    GameObject obj = Instantiate(EnemyIce, new Vector3(7.85f, y, 0), Quaternion.identity);
-                    GameManager.instance.AdicionarInimigos(obj);
-                }
-
-                else if (tipoInimigo > 9)
-                {
-                    GameObject obj = Instantiate(EnemyRock, new Vector3(7.85f, y, 0), Quaternion.identity);
-                    GameManager.instance.AdicionarInimigos(obj);
-                }
-
-                else if (tipoInimigo > 8)
-                {
-                    GameObject obj = Instantiate(EnemyLight, new Vector3(7.85f, y, 0), Quaternion.identity);
-                    GameManager.instance.AdicionarInimigos(obj);
-                }
-
-                
             }
 
-        }
+            else if (spawnPlace == 2)
+            {
+                    y = -0.23f;
+            }
 
-        if (timer > 0)
-        {
-            timer -= Time.deltaTime;
+            else if (spawnPlace == 3)
+            {
+                    y = -3.27f;
+            }
 
-        }
-        
+            tipoInimigo = Random.Range(0, 100);
 
+            if (tipoInimigo > 70)
+            {
+                    GameObject obj = Instantiate(EnemyFire, new Vector3(7.85f, y, 0), Quaternion.identity);
+                    GameManager.instance.AdicionarInimigos(obj);
+            }
+
+            else if (tipoInimigo > 50)
+            {
+                    GameObject obj = Instantiate(EnemyDark, new Vector3(7.85f, y, 0), Quaternion.identity);
+                    GameManager.instance.AdicionarInimigos(obj);
+            }
+
+            else if (tipoInimigo > 10)
+            {
+                    GameObject obj = Instantiate(EnemyIce, new Vector3(7.85f, y, 0), Quaternion.identity);
+                    GameManager.instance.AdicionarInimigos(obj);
+            }
+
+            else if (tipoInimigo > 9)
+            {
+                    GameObject obj = Instantiate(EnemyRock, new Vector3(7.85f, y, 0), Quaternion.identity);
+                    GameManager.instance.AdicionarInimigos(obj);
+            }
+
+            else if (tipoInimigo > 8)
+            {
+                    GameObject obj = Instantiate(EnemyLight, new Vector3(7.85f, y, 0), Quaternion.identity);
+                    GameManager.instance.AdicionarInimigos(obj);
+            }
+
+            inimigosSpawnados++; //Amenta o contador de inimigos spawnados;
+            tempoDeSpawn = 1.0f; //Tempo entre cada spawn de inimigo;
+
+            if (inimigosSpawnados >= tamanhoDoGrupo) // Quando 10 inimigos forem spawnados, reinicia o grupo
+            {
+                inimigosSpawnados = 0;
+
+                tempoDeSpawn = 10f; //Tempo de spawn de cada grupo;
+                
+                if (tempoDeSpawn > 0) //reduz o tempo de espera entyre os spawns;
+                {
+                    tempoDeSpawn  -= Time.deltaTime;
+                }
+            }
+       } 
 
     }
 
